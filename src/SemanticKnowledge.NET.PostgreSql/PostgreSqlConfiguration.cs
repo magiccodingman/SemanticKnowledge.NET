839f114b2ed92a6158a55f92d3c0a09bf2a0e2e3
Magic <131926685+magiccodingman@users.noreply.github.com>
2026-08-12 14:41:43 -0400

    fix(postgres): import Npgsql vector extension namespace

--- src/SemanticKnowledge.NET.PostgreSql/PostgreSqlConfiguration.cs
+++ src/SemanticKnowledge.NET.PostgreSql/PostgreSqlConfiguration.cs
@@ -1,6 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
 using OnnxTextEmbeddings.PgVector;
-using Pgvector;
 
 namespace SemanticKnowledge.PostgreSql;
 
@@ -22,7 +22,7 @@ public static class SemanticKnowledgePostgreSqlExtensions
         configure(options);
         if (string.IsNullOrWhiteSpace(options.ConnectionString)) throw new InvalidOperationException("PostgreSQL ConnectionString is required.");
         if (string.IsNullOrWhiteSpace(options.Schema)) throw new InvalidOperationException("PostgreSQL Schema is required.");
-        var dataSourceBuilder = new Npgsql.NpgsqlDataSourceBuilder(options.ConnectionString);
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(options.ConnectionString);
         dataSourceBuilder.UseVector();
         builder.Services.AddSingleton(options);
         builder.Services.AddSingleton(dataSourceBuilder.Build());
