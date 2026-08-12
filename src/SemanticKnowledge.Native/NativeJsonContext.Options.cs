01374ff1ec6a9b234beae2f0a43553b95ba056c5
Magic <131926685+magiccodingman@users.noreply.github.com>
2026-08-12 15:12:46 -0400

    fix(native): make ABI JSON casing language-neutral

---
+++ src/SemanticKnowledge.Native/NativeJsonContext.Options.cs
@@ -0,0 +1,8 @@
+using System.Text.Json.Serialization;
+
+namespace SemanticKnowledge.Native;
+
+[JsonSourceGenerationOptions(
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    PropertyNameCaseInsensitive = true)]
+internal partial class NativeJsonContext;
